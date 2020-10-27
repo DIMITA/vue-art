fcf7ac306874f98ce66b371e29af4d6018c4cbf0
OpenSI <dimitriomorales01@gmail.com>
2020-10-27 16:46:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const prog = require('caporal');
+const fs = require('fs')
+const path = require('path')
+const ncp = require('ncp').ncp
+
+ncp.limit = 16
+
+prog
+    .version('1.0.0')
+
+.command('make', 'Generate some tempate')
+
+.argument('<type>', 'type of template that you want to generate', /^store|page|component$/)
+    .argument('[name]', 'Environment to deploy on')
+
+.option('--skip-test <lines>', 'Skip test')
+
+.action(function(args, options, logger) {
+    if (args.type === 'page' || args.type === "component") {
+
+        const entryPath = path.resolve(__dirname, 'templates', args.type + '.txt')
+        const entryContent = fs.readFileSync(entryPath)
+        fs.writeFile('./src/' + args.type + 's/' + args.name + '.vue', entryContent, (err) => { logger.error(err) })
+
+    } else {
+
+        ncp(path.resolve(__dirname, 'templates', args.type), path.resolve('./src', args.type, args.name), function(err) {
+            err ? logger.error(err) : logger.info(`the ${args.type} ${args.name} was generated`)
+
+        })
+
+    }
+
+
+
+});
+
+prog.parse(process.argv);
